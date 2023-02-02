2fae5e32c1256676b5aee9e74a9d3bfcf8e9b258
Nikos Karampas <nikos.karampas@hotmail.com>
2023-02-02 19:26:19 +0200

    Added CustomerImageController, CustomerImageService, impemented UploadImageAsync and GetImageAsync (plus the endpoints)

---
+++ Customers.Api/ImageSettings/BucketSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Customers.Api.ImageSettings
+{
+    public class BucketSettings
+    {
+        public const string Key = "Bucket";
+
+        public required string Name { get; set; }
+    }
+}
---
+++ Customers.Api/Services/CustomerImageService.cs
@@ -0,0 +1,53 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using Customers.Api.ImageSettings;
+using Microsoft.Extensions.Options;
+
+namespace Customers.Api.Services
+{
+    public class CustomerImageService : ICustomerImageService
+    {
+        private readonly IAmazonS3 _s3Client;
+        private readonly IOptions<BucketSettings> _bucketSettings;
+
+        public CustomerImageService(IAmazonS3 s3Client, IOptions<BucketSettings> bucketSettings)
+        {
+            _s3Client = s3Client;
+            _bucketSettings = bucketSettings;
+        }
+
+        public async Task<PutObjectResponse> UploadImageAsync(Guid id, IFormFile file)
+        {
+            var putObjectRequest = new PutObjectRequest
+            {
+                BucketName = _bucketSettings.Value.Name,
+                Key = $"images/{id}",
+                ContentType = file.ContentType,                
+                InputStream = file.OpenReadStream(),
+                Metadata =
+                {
+                    ["x-amz-meta-originalname"] = file.FileName,
+                    ["x-amz-meta-extension"] = Path.GetExtension(file.FileName)
+                }
+            };
+
+            return await _s3Client.PutObjectAsync(putObjectRequest);
+        }
+
+        public async Task<GetObjectResponse> GetImageAsync(Guid id)
+        {
+            var getObjectRequest = new GetObjectRequest
+            {
+                BucketName = _bucketSettings.Value.Name,
+                Key = $"images/{id}"
+            };
+
+            return await _s3Client.GetObjectAsync(getObjectRequest);
+        }
+
+        public async Task<DeleteObjectResponse> DeleteImageAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

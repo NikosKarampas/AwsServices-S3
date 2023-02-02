1e8d5901c57970cc698d767f3845d0db131f97a0
Nikos Karampas <nikos.karampas@hotmail.com>
2023-02-02 17:27:56 +0200

    Added Customers.Api

---
+++ Customers.Api/Repositories/CustomerRepository.cs
@@ -0,0 +1,124 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Runtime.Internal.Transform;
+using Customers.Api.Contracts.Data;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using System.Net;
+using System.Text.Json;
+
+namespace Customers.Api.Repositories;
+
+public class CustomerRepository : ICustomerRepository
+{
+    private readonly IAmazonDynamoDB _dynamoDB;
+    private readonly string _tableItem = "customers";
+
+    public CustomerRepository(IAmazonDynamoDB dynamoDB)
+    {        
+        _dynamoDB = dynamoDB;
+    }
+
+    public async Task<bool> CreateAsync(CustomerDto customer)
+    {
+        customer.UpdatedAt = DateTime.UtcNow;
+        var customerAsJson = JsonSerializer.Serialize(customer);
+        var customerAsAttributes = Document.FromJson(customerAsJson).ToAttributeMap();
+
+        var createItemRequest = new PutItemRequest
+        {
+            TableName = _tableItem,
+            Item = customerAsAttributes
+        };
+
+        var response = await _dynamoDB.PutItemAsync(createItemRequest);
+        return response.HttpStatusCode == HttpStatusCode.OK;
+    }
+
+    public async Task<CustomerDto?> GetAsync(Guid id)
+    {
+        var getItemRequest = new GetItemRequest
+        {
+            TableName = _tableItem,            
+            Key = new Dictionary<string, AttributeValue>
+            {
+               { "pk", new AttributeValue { S = id.ToString() } },
+               { "sk", new AttributeValue { S = id.ToString() } },
+            }
+        };
+
+        var response = await _dynamoDB.GetItemAsync(getItemRequest);
+
+        if (response.Item.Count == 0)
+            return null;
+
+        var itemAsDocument = Document.FromAttributeMap(response.Item);
+        return JsonSerializer.Deserialize<CustomerDto?>(itemAsDocument.ToJson());
+    }
+
+    public async Task<CustomerDto?> GetByEmailAsync(string email)
+    {
+        var queryRequest = new QueryRequest
+        {
+            TableName = _tableItem,                     
+            IndexName = "Email-id-index",
+            KeyConditionExpression = "Email = :v_Email",
+            ExpressionAttributeValues = new Dictionary<string, AttributeValue>
+            {
+                {
+                    ":v_Email", new AttributeValue { S = email }
+                }
+            }
+        };
+
+        var response = await _dynamoDB.QueryAsync(queryRequest);
+
+        if (response.Items.Count == 0)
+            return null;
+
+        var itemAsDocument = Document.FromAttributeMap(response.Items[0]);
+        return JsonSerializer.Deserialize<CustomerDto>(itemAsDocument.ToJson());
+    }
+
+    public async Task<IEnumerable<CustomerDto>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> UpdateAsync(CustomerDto customer, DateTime requestStarted)
+    {
+        customer.UpdatedAt = DateTime.UtcNow;
+        var customerAsJson = JsonSerializer.Serialize(customer);
+        var customerAsAttributes = Document.FromJson(customerAsJson).ToAttributeMap();
+
+        var updateItemRequest = new PutItemRequest
+        {
+            TableName = _tableItem,
+            Item = customerAsAttributes,
+            ConditionExpression = "UpdatedAt < :requestStarted", //if updatedAt of server is greater than requeststarted then throw exception
+            ExpressionAttributeValues = new Dictionary<string, AttributeValue>
+            {
+                { ":requestStarted", new AttributeValue { S = requestStarted.ToString("O") } }
+            }
+        };
+
+        var response = await _dynamoDB.PutItemAsync(updateItemRequest);
+        return response.HttpStatusCode == HttpStatusCode.OK;
+    }
+
+    public async Task<bool> DeleteAsync(Guid id)
+    {
+        var deleteItemRequest = new DeleteItemRequest
+        {
+            TableName = _tableItem,
+            Key = new Dictionary<string, AttributeValue>
+            {
+               { "pk", new AttributeValue { S = id.ToString() } },
+               { "sk", new AttributeValue { S = id.ToString() } },
+            }
+        };
+
+        var response = await _dynamoDB.DeleteItemAsync(deleteItemRequest);
+        return response.HttpStatusCode == HttpStatusCode.OK;
+    }    
+}
